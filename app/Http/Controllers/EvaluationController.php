a926bf17b29798429e85e117fec985d51367a75c
Junior24 <junioskyjustin@gmail.com>
2024-06-06 17:02:54 +0100

    creation du model evaluation et configuration de phpexcel

---
+++ app/Http/Controllers/EvaluationController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Evaluation;
+use Illuminate\Http\Request;
+
+class EvaluationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Evaluation $evaluation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Evaluation $evaluation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Evaluation $evaluation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Evaluation $evaluation)
+    {
+        //
+    }
+}
--- routes/web_rapport.php
+++ routes/web_rapport.php
@@ -1,4 +1,6 @@
 <?php
 
+use App\Http\Controllers\EvaluationController;
 use Illuminate\Support\Facades\Route;
 
+Route::resource("evaluations", EvaluationController::class);
