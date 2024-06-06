75892df11ac2159446704a887cb54c114eed6eee
mobutu <mkmathias06@gmail.com>
2024-06-06 17:27:04 +0100

    modification dans le fichier index

--- resources/views/employabilite/index.blade.php
+++ resources/views/employabilite/index.blade.php
@@ -48,6 +48,7 @@
                     <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
 
                         <td class="px-6 py-4">
+                            {{ $item->name }}
                         </td>
                         <td class="px-6 py-4">
                             {{ substr($item->type_contrat, 0, 100) }}
