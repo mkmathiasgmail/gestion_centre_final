e89e4697e1796ea457a86a1dacc1d5c88772862f
mobutu <mkmathias06@gmail.com>
2024-06-06 10:40:34 +0100

    creation migration route pour l'employabilite

---
+++ app/Http/Controllers/EmplayabiliteController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Emplayabilite;
+use App\Http\Requests\StoreEmplayabiliteRequest;
+use App\Http\Requests\UpdateEmplayabiliteRequest;
+
+class EmplayabiliteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreEmplayabiliteRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Emplayabilite $emplayabilite)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Emplayabilite $emplayabilite)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateEmplayabiliteRequest $request, Emplayabilite $emplayabilite)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Emplayabilite $emplayabilite)
+    {
+        //
+    }
+}
