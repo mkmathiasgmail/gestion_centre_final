393e038617ca33671aed4fe9cd02d6aa36331126
Vincent-Tshipamba <tshipambalubobo80@gmail.com>
2024-06-11 15:15:37 +0100

    Retrait des importations non ut iles à la récupération et affichage des données

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,12 +2,10 @@ import './bootstrap';
 import 'flowbite';
 import Alpine from 'alpinejs';
 import Quill from 'quill';
-import DataTable from 'datatables.net-dt';
 
 window.Alpine = Alpine;
 
 Alpine.start();
 
-let table = new DataTable('#myTable', {
-    responsive: true
-});
+
+
