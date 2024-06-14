a52a04a16dd58a29d30b131aa8215d6a09d8b1ed
Musongela <musongelacarlo54@gmail.com>
2024-06-14 14:23:23 +0100

    mise a jour consol commande fetchdata gestion activites

--- resources/views/activites/show.blade.php
+++ resources/views/activites/show.blade.php
@@ -108,7 +108,7 @@
                 <h2 class=" text-5xl">{{ $show->title }}</h2>
                 <img src="{{ $show->image }}" alt="" class=" rounded-md mt-5 mb-5">
                 <span class="rounded-full mt-5 mb-5 bg-slate-600 pr-4 pl-4 pt-1 pb-1 text-3xl font-bold">{{$show->categorie->categorie}}</span>
-                <p class="mt-5 mb-5">{{ $show->description }}</p>
+                <p class="mt-5 mb-5">{{ $show->content }}</p>
             </div>
         </div>
 
