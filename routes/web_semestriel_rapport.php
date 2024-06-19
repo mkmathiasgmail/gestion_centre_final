f18b959d1253858b146eca170551e5fd8669a48c
Manasse_Tshims <manassetshims@gmail.com>
2024-06-19 15:53:17 +0100

    ajustement de la navigation, du fichier evaluation/index.blade.php et de web_semestriel_rapport

--- routes/web_semestriel_rapport.php
+++ routes/web_semestriel_rapport.php
@@ -1,8 +1,8 @@
 <?php
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TypeEventController;
 use App\Http\Controllers\RapportSemestrielController;
 
 Route::get("rapportSemestriel/exportSemestriel", [RapportSemestrielController::class, "exportToExcel"])->name("exportSemestre");
-Route::post("typeEvents","getEvents")->name('get-Events');
 
 Route::resource('rapportSemestriel', RapportSemestrielController::class);
