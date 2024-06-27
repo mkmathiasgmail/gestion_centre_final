8c54352d6f6bc57ac004d59ef7dcb760a9f2d403
Musongela <musongelacarlo54@gmail.com>
2024-06-27 12:22:06 +0100

    update file migration for a code better

--- resources/views/components/activitesShow.blade.php
+++ resources/views/components/activitesShow.blade.php
@@ -6,7 +6,7 @@
     aria-labelledby="profile-tab">
     <div class=" text-white">
         <h2 class=" text-5xl">{{ $show->title }}</h2>
-        <img src="{{ $show->image }}" alt="" class=" rounded-md mt-5 mb-5">
+        <img src="{{ $show->thumbnail_url }}" alt="" class=" rounded-md mt-5 mb-5">
         <span
             class="rounded-full mt-5 mb-5 bg-slate-600 pr-4 pl-4 pt-1 pb-1 text-3xl font-bold">{{ $show->categorie->categorie }}</span>
         <p class="mt-5 mb-5">{{ $show->content }}</p>
