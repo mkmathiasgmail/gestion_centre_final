8153e7bf35322cc2b5e91ce39f17086937363e20
francine <magbiafr2222@gmail.com>
2024-06-28 20:16:15 +0100

    Improving the displaying of the data table 'presences'

---
+++ resources/views/presences/confirInfo.blade.php
@@ -0,0 +1,46 @@
+<x-app-layout>
+
+
+    <form class="max-w-sm mx-auto" action="{{ route('presences.store') }}" method="post">
+        @csrf
+        
+        @if (isset($nom) && isset($prenom))
+            <div id="userInfo">
+                <div class="mb-5">
+                    <label for="firstname"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Prenom</label>
+                    <input type="text" id="firstname" name="firstname" value="{{ $prenom }}"
+                        class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                        readonly />
+                </div>
+                <div class="mb-5">
+                    <label for="lastname"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nom</label>
+                    <input type="text" id="lastname" name="lastname" value="{{ $nom }}"
+                        class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                        readonly />
+                </div>
+                {{-- <div class="mb-5">
+                    <label for="activite_id"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Id_Activite</label>
+                    <input type="text" id="activite_id" name="activite_id" value="{{ $activite_id }}"
+                        class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+                        readonly />
+                </div> --}}
+                <button type="submit"
+                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Valider</button>
+            </div>
+        @endif
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+    </form>
+
+
+</x-app-layout>
