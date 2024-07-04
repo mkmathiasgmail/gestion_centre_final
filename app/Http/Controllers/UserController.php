9d78a25ad3581fe8bf85e59ac5c7c55942ec13fb
mobutu <mkmathias06@gmail.com>
2024-07-04 15:10:48 +0100

    "creation des fichiers sept pour la permission de role "

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,76 @@
+<?php
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Session;
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+
+            $users = User::all();
+            return view('user.index',compact('users'));
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+
+            $roles = Role::all();
+            $user = User::find($id);
+
+            $user_roles = $user->getRoleNames();
+            $user_role = [];
+            foreach ($user_roles as $role) {
+                $user_role[] = $role;
+            }
+           
+            return view('user.detail',compact('roles','user','user_role'));
+
+    }
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
+
---
+++ app/Http/Middleware/PermissionUser.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\Models\User;
+use Illuminate\Http\Request;
+use PhpParser\Node\Stmt\Else_;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class PermissionUser
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+
+            $user = $request->user();
+
+            if($user->hasRole('admin')){
+            return $next($request);
+            }
+
+            return redirect()->route('activitencours');
+
+
+
+    }
+}
