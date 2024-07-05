9810a852f49ef9e185387d3aa25ff560d71ac70c
josue Kristo <josuekristo5@gmail.com>
2024-07-05 15:25:47 +0100

    my new commit for updating import xlsx script

--- app/Models/Presence.php
+++ app/Models/Presence.php
@@ -10,11 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Presence extends Model
 {
     use HasFactory;
-<<<<<<< HEAD
-    protected $fillable=['date','candidat_id'];
-=======
     protected $fillable=['date', 'candidat_id'];
->>>>>>> 03360cbbd4006de2704fd44990b6cfcd5217be27
    
     public function candidat():BelongsTo
     {
