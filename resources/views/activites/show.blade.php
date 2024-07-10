1344043f3d089545fa59a5973d3251a6f2f8d806
josue Kristo <josuekristo5@gmail.com>
2024-07-10 16:25:39 +0100

    ajout du fichier modifier dans show.activites

--- resources/views/activites/show.blade.php
+++ resources/views/activites/show.blade.php
@@ -352,7 +352,7 @@
         </div>
         <div class="hidden p-4 ro unded-lg bg-gray-50 dark:bg-gray-800" id="import" role="tabpanel"
         aria-labelledby="contacts-tab">
-        <p class="text-sm text-gray-500 dark:text-gray-400"><x-activite-import/></p>
+        <p class="text-sm text-gray-500 dark:text-gray-400"><x-activite-import :activite="$activite"/></p>
     </div>
     </div>
 
