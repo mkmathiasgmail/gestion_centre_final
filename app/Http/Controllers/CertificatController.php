6423ef589dfc9ecae41b6166660a4c71ba80fd49
Manasse_Tshims <manassetshims@gmail.com>
2024-07-11 13:49:29 +0100

    creation du controller , model, et migration pour la creation des certificats

---
+++ app/Http/Controllers/CertificatController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Certificat;
+use Illuminate\Http\Request;
+
+class CertificatController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Certificat $certificat)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Certificat $certificat)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Certificat $certificat)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Certificat $certificat)
+    {
+        //
+    }
+}
