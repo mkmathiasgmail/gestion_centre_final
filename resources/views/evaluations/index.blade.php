7c3c7a3472a3b1d6b70e02e673d39dfed6701a48
Jeremie Ngovula <jeremiengovula77@gmail.com>
2024-07-17 11:20:35 +0100

    Ajout de la route pour l'exportation du calendrier

--- resources/views/evaluations/index.blade.php
+++ resources/views/evaluations/index.blade.php
@@ -367,7 +367,7 @@
     class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
     <h5 class=" mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Calendrier des activités</h5>
    
-    <a href="{{ route('calexport') }}" data-modal-target="semetrielrapport-modal"
+    <a href="{{ route('calexport') }}" data-modal-target="calexport"
         data-modal-toggle="calexport" 
         class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
         Générer
