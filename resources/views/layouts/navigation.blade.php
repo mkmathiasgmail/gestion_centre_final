26c6ae60bd1d16b2a59a3defbb0e66854490c97f
Musongela <musongelacarlo54@gmail.com>
2024-07-26 15:48:06 +0100

    mise en place de la suppresssion de l'activites en local et vers l'api

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -116,7 +116,7 @@
                                 <p class="mb-2 text-black hover:text-gray dark:text-gray-400"><a
                                         href="{{ route('activites.index') }}">Actvités</a></p>
                                 <p class="mb-2 text-black hover:text-gray dark:text-gray-400"><a
-                                        href="{{route('categories.index')}}">Catégories</a></p>
+                                        href="{{ route('categories.index') }}">Catégories</a></p>
                                 <p class="mb-2 text-black hover:text-gray dark:text-gray-400"><a
                                         href="{{ route('hashtags.index') }}">Hashtag</a></p>
                                 <p class="mb-2 text-black hover:text-gray dark:text-gray-400"><a
@@ -157,20 +157,20 @@
 
 
                 @if (Auth()->user()->hasRole('super-admin'))
-                <li>
-                    <a href="{{ route('user_role.index') }}"
-                        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
-                            viewBox="0 0 24 24">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M12 21a9 9 0 1 0 0-18 9 9 0 0 0 0 18Zm0 0a8.949 8.949 0 0 0 4.951-1.488A3.987 3.987 0 0 0 13 16h-2a3.987 3.987 0 0 0-3.951 3.512A8.948 8.948 0 0 0 12 21Zm3-11a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                        </svg>
-                        <span class="ms-3">Role</span>
+                    <li>
+                        <a href="{{ route('user_role.index') }}"
+                            class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M12 21a9 9 0 1 0 0-18 9 9 0 0 0 0 18Zm0 0a8.949 8.949 0 0 0 4.951-1.488A3.987 3.987 0 0 0 13 16h-2a3.987 3.987 0 0 0-3.951 3.512A8.948 8.948 0 0 0 12 21Zm3-11a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                            </svg>
+                            <span class="ms-3">Role</span>
 
-                    </a>
-                </li>
+                        </a>
+                    </li>
                 @endif
                 <li>
                     <a href="{{ route('import') }}"
@@ -201,18 +201,6 @@
                     </a>
                 </li>
             </ul>
-                <li>
-                    <a href="{{ route('send.email') }}"
-                        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-  <path fill-rule="evenodd" d="M4 3a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h1v2a1 1 0 0 0 1.707.707L9.414 13H15a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H4Z" clip-rule="evenodd"/>
-  <path fill-rule="evenodd" d="M8.023 17.215c.033-.03.066-.062.098-.094L10.243 15H15a3 3 0 0 0 3-3V8h2a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-1v2a1 1 0 0 1-1.707.707L14.586 18H9a1 1 0 0 1-.977-.785Z" clip-rule="evenodd"/>
-</svg>
-
-
-                        <span class="ms-3">Email</span>
-                    </a>
-                </li>
         </div>
     </aside>
 </div>
