83d26e36f99613ec419d836212413f0e15a8bf99
JuximusXR <juniorwalkermcclain@gmail.com>
2024-08-06 16:31:40 +0100

    ajout des permission

--- routes/web_notification.php
+++ routes/web_notification.php
@@ -4,7 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MailController;
 use App\Http\Controllers\NotificationController;
 
-Route::get('notifications/sendMail', [MailController::class, 'sendMail'])->name('sendMail');
-Route::get('notifications/autocomplete', [MailController::class, 'autocomplete'])->name('autocomplete');
+Route::middleware('auth')->group(function () {
+    Route::get('notifications/sendMail', [MailController::class, 'sendMail'])->name('sendMail');
+    Route::get('notifications/autocomplete', [MailController::class, 'autocomplete'])->name('autocomplete');
+    Route::resource('notifications', NotificationController::class);
+})->middleware('permissions');
 
-Route::resource('notifications', NotificationController::class);
+require __DIR__ . '/auth.php';
+
