346f17ffae823fe4106c167f79bfbb956b6050ca
Pathel7 <mpoleshapaulin70@gmail.com>
2024-08-15 11:20:40 +0100

    Ajout du fichier coursera.blade.php

---
+++ resources/views/components/coursera.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>form-csv-import</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <h1 class="m-8 text-center text-gray-500">Importation des fichiers csv</h1>
+    {{$slot}}
+</body>
+</html>
