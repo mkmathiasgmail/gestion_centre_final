661317325a7889b9dc6b305c22dfb78f514eb68f
Manasse_Tshims <manassetshims@gmail.com>
2024-08-26 18:55:47 +0100

    optimisation du rapport semestrielle pour gerer le telechargement avec ajax, ajout de la methode ajax dans la page index/evaluations, ajout de la route pour la methode de génération des plusieurs certificat

--- routes/web_certificat.php
+++ routes/web_certificat.php
@@ -7,5 +7,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/certificatgenerer/{tshor}', [CertificatController::class, 'generateCertificat'])->name('certificat');
 
 Route::get('/generateAllCertificat/{id}', [CertificatController::class, 'generateAllCertificat'])->name('allCertificat');
+Route::get('/vuecertificat/{id}', [CertificatController::class, 'vuecertificat'])->name('voircertificat');
 
 Route::resource('/certificats', CertificatController::class);
