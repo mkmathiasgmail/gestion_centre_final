7f9ab8ce1fa748a86629477933886f4e956c7af4
Jeremie Ngovula <jeremiengovula77@gmail.com>
2024-08-26 17:42:32 +0100

    Ajout des style

--- resources/views/evaluations/index.blade.php
+++ resources/views/evaluations/index.blade.php
@@ -299,16 +299,13 @@
                 <!---Calendrier Rapport---->
 
                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-
-                    <td class="px-6 py-4"> 4 </td>
-
-                    <td class="px-6 py-4"> Calendrier des activités </td>
-
-                    <td class="px-6 py-4"> </td>        
-
+                    <td class="px-6 py-4">4</td>
+                    <td class="px-6 py-4">Calendrier des activités</td>
+                    <td class="px-6 py-4"></td>
                     <td class="px-6 py-4">
-                        <a href="{{ route('calexport') }}" data-modal-target="calexport"
-                            data-modal-toggle="calexport" 
+                        <a href="{{ route('calexport') }}" data-modal-target="calendrier-modal"
+                            onclick="genererCalendrier(event)"
+                            data-modal-toggle="calendrier-modal"
                             class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             Générer
                             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
@@ -317,13 +314,71 @@
                                     d="M1 5h12m0 0L9 1m4 4L9 9" />
                             </svg>
                         </a>  
+                        <!-- Modal -->
+                        <div id="calendrier-modal" tabindex="-1" aria-hidden="true"
+                            class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                            <div class="relative p-4 w-full max-w-md max-h-full">
+                                <!-- Modal content -->
+                                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                    <!-- Modal header -->
+                                    <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                                            Selectionner le semestre
+                                        </h3>
+                                        <button type="button"
+                                            class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                            data-modal-toggle="calendrier-modal">
+                                            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                                fill="none" viewBox="0 0 14 14">
+                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                                    stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                            </svg>
+                                            <span class="sr-only">Close modal</span>
+                                        </button>
+                                    </div>
+                                    <!-- Modal body -->
+                                 
+                                    <form action="" method="post" class="p-4 md:p-5" id="form">
+                                        @method('GET')
+                                        <div class="flex justify-center space-x-4">
+                                            <div class="col-span-2 sm:col-span-1">
+                                                <label for="periode"
+                                                    class="flex justify-center mb-2 text-sm font-medium text-gray-900 dark:text-white">Sélectionner le
+                                                    Semestre</label>
+                                                <select id="periode" name="periode"
+                                                    class="justify-center bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                                                    <option value="Semestre_1">Semestre_1</option>
+                                                    <option value="Semestre_2">Semestre_2</option>
+                                                </select>
+                                            </div>
+                                            <div>
+                                                <label for="anneeSelect"
+                                                    class="flex justify-center mb-2 text-sm font-medium text-gray-900 dark:text-white">Sélectionner l'année</label>
+                                                <select id="anneeSelect" name="anneeSelect"
+                                                    class="justify-center bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="flex justify-center mt-6">
+                                            <button type="submit"
+                                                class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                                <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                                                    xmlns="http://www.w3.org/2000/svg">
+                                                    <path fill-rule="evenodd"
+                                                        d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                                                        clip-rule="evenodd"></path>
+                                                </svg>
+                                                Generer
+                                            </button>
+                                        </div>
+                                    </form>
+
+                                </div>
+                            </div>
+                        </div>
                     </td>
                 </tr>
-
-            </tbody>
-        </table>
-    </div>
-
+                
         <!---Wenzory rapport---->
         
         {{-- <div
@@ -535,6 +590,54 @@
             
             // Attach generateYearOptions to DOMContentLoaded for immediate execution
             document.addEventListener('DOMContentLoaded', generateYearOptions);
+
+            //code pour le calendrier 
+
+            function genererCalendrier(event) {
+                event.preventDefault();
+                const newAction = event.currentTarget.href; // Utiliser `currentTarget` pour plus de sécurité
+                const form = document.querySelector("#calendrier-modal #form"); // Utiliser l'ID correct du formulaire dans le modal
+                
+                if (form) {
+                    form.action = newAction;
+                } else {
+                    console.error("Formulaire non trouvé.");
+                }
+            }
+
+            // Fonction améliorée pour générer les options de l'année
+            function anneeOptions() {
+                const yearSelect = document.getElementById('anneeSelect');
+
+                if (!yearSelect) {
+                    console.error("Élément select avec l'ID 'yearselec' introuvable.");
+                    return;
+                }
+
+                const currentYear = new Date().getFullYear();
+                const yearRange = 15; // 7 années passées + l'année actuelle + 7 années futures
+
+                const years = Array.from({ length: yearRange }, (_, index) => currentYear - 7 + index);
+
+                years.forEach(year => {
+                    const option = new Option(year, year, year === currentYear);
+                    yearSelect.add(option);
+                });
+            }
+
+            function genererSem(event) {
+                event.preventDefault();
+                const lien = event.target.getAttribute("href");
+                document.querySelector("#calendrier-modal #form").setAttribute("action", lien);
+            }
+
+
+            // Attacher l'événement pour exécuter la fonction lors du chargement du DOM
+            document.addEventListener('DOMContentLoaded', () => {
+                anneeOptions();
+            });
+            
+           
         </script>
     @endsection
 </x-app-layout>
