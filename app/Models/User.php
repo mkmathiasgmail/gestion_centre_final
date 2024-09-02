96a93152f3b0b464040a1a0676b96377d651203f
Vincent Tshipamba <tshipambalubobo80@gmail.com>
2024-09-02 15:51:21 +0100

    Retrait du trait UUID pour le modele User afin pour des raisons de conflit avec le nommage par defaut utilisé par Laravel Spatie

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable , HasRoles, HasUuids;
+    use HasFactory, Notifiable , HasRoles;
 
     protected $fillable = [
         'name',
