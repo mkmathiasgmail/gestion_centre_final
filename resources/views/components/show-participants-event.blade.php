8e127817d487cbd622754b04afb713343319a736
Vincent Tshipamba <tshipambalubobo80@gmail.com>
2024-09-05 13:06:57 +0100

    Organisation du tableau d'affichage des participants

--- resources/views/components/show-participants-event.blade.php
+++ resources/views/components/show-participants-event.blade.php
@@ -125,13 +125,13 @@
                         @endif
                     @endforeach
                     <th scope="col" class="px-6 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        Certificat
+                    </th>
+                    <th scope="col" class="px-6 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                         Status
                     </th>
                     <th scope="col" class="px-6 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                         Actions
-                    </th>
-                    <th scope="col" class="px-6 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        Certificat
                     </th>
                 </tr>
             </thead>
