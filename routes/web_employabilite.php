f5663f076879611fa22b2a15edaf000a452934f0
mobutu <mkmathias06@gmail.com>
2024-09-05 16:40:23 +0100

    creation du fichier CreateSuperAdmin.php pour generer un user admin par un commande artisan & modification du fichier web & optimisation des view

--- routes/web_employabilite.php
+++ routes/web_employabilite.php
@@ -8,6 +8,8 @@ use App\Http\Controllers\TypeContratController;
 use App\Http\Controllers\EmployabiliteController;
 
 
+Route::get('get_emplois/{id}', [EmployabiliteController::class, 'getEmplois'])->name('getdataEmploye');
+
 Route::resource('type_Contrats', TypeContratController::class);
 
 Route::resource('/user_role',UserController::class)->middleware('permissions:super-admin');
