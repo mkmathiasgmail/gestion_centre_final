758f72cc409c14c947341ba0c3b9647797676bf0
mcleroi01 <musongelacarlo54@gmail.com>
2024-09-06 11:23:55 +0100

    navigation gestion center  modification sidebarmax

--- resources/views/components/statusdesactive.blade.php
+++ resources/views/components/statusdesactive.blade.php
@@ -1,7 +1,7 @@
 @props(['activiteid', 'name'])
 
 <div id="desactive-{{ $activiteid }}" tabindex="-1"
-    class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    class="hidden ab overflow-y-auto overflow-x-hidden fixed bottom-1/2 right-1/2 left-1/2 top-1/2 z-100 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
     <div class="relative p-4 w-full max-w-md max-h-full">
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
             <button type="button"
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,7 @@
             <div class=" dark:bg-[#1e293bf3] bg-[#eaeaebf3] pt-20 px-2 md:px-5 pb-4 ml-12  backdrop-blur-2xl">
                 <!-- Page Heading -->
                 @if (isset($header))
-                    <nav class = "flex px-5 py-3 text-gray-700  rounded-lg bg-gray-50 dark:bg-[#1E293B] "
+                    <nav class = "flex px-5 py-3 text-gray-700  rounded-lg bg-[#eaeaebf3] dark:bg-[#1E293B] "
                         aria-label="Breadcrumb">
                         <ol class = "inline-flex items-center space-x-1 md:space-x-3">
                             <li class = "inline-flex items-center">
@@ -56,8 +56,7 @@
                                             d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
                                             clipRule="evenodd"></path>
                                     </svg>
-                                    <a href="#"
-                                        class = "ml-1 text-sm font-medium text-gray-700 hover:text-gray-900 md:ml-2 dark:text-gray-400 dark:hover:text-white">Templates</a>
+                                    
                                 </div>
                             </li>
                         </ol>
