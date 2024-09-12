689923bccd2f3e2275df0011980eb71afc0a252c
francine <magbiafr2222@gmail.com>
2024-09-12 14:53:55 +0100

    Ajout du code de couleur de l'orange Digital Center et Modification des routes pour la présence.

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,10 @@ export default {
             fontFamily: {
                 sans: ["Figtree", ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                odcolor: "#ff7900",
+
+            }
         },
     },
 
