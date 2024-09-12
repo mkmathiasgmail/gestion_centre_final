6f849d40ad908e083da0a85c8fcd08f036daed92
mobutu <mkmathias06@gmail.com>
2024-09-12 15:02:03 +0100

    suppression du tag middleware dans le fichiers de route web & optimisation de vue pour ajout d'un employé & creation d'un fichier de migration d'ajout de colonne pour employabilites

--- resources/views/components/listChoice.blade.php
+++ resources/views/components/listChoice.blade.php
@@ -1,20 +1,21 @@
-<!-- Large Modal -->
-<div id="large-employé" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
-    <div class="relative w-full max-w-4xl max-h-full">
+<!-- Main modal -->
+<div id="large-employe-modal" tabindex="-1" aria-hidden="true"
+    class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="relative w-full max-w-2xl max-h-full p-4">
         <!-- Modal content -->
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
             <!-- Modal header -->
             <div class="flex items-center justify-between p-4 border-b rounded-t md:p-5 dark:border-gray-600">
-
-
-
-                <h3 class="text-xl font-medium text-gray-900 dark:text-white">
-                    parcours d'un employé
+                <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                    Parcours Professionnel d'un employé
                 </h3>
-                <button id="resetButton2" type="button" class="inline-flex items-center justify-center w-8 h-8 text-sm text-gray-400 bg-transparent rounded-lg hover:bg-gray-200 hover:text-gray-900 ms-auto dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="large-employé">
-
-                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                <button type="button"
+                    class="inline-flex items-center justify-center w-8 h-8 text-sm text-gray-400 bg-transparent rounded-lg hover:bg-gray-200 hover:text-gray-900 ms-auto dark:hover:bg-gray-600 dark:hover:text-white"
+                    data-modal-hide="large-employe-modal">
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                     </svg>
                     <span class="sr-only">Close modal</span>
                 </button>
@@ -22,7 +23,8 @@
             <!-- Modal body -->
             <div class="p-4 space-y-4 md:p-5">
                 <div class="relative mt-4 overflow-x-auto">
-                    <table class="w-full text-sm text-left text-gray-500 rtl:text-right dark:text-gray-400 display " style="width: 100%" id="Mytable">
+                    <table class="w-full text-sm text-left text-gray-500 rtl:text-right dark:text-gray-400 display "
+                        style="width: 100%" id="Mytable">
                         <thead class="text-xs text-white uppercase bg-gray-50 dark:bg-gray-700 dark:text-white">
                             <tr>
                                 <th scope="col" class="px-6 py-3 bg-slate-700">
@@ -51,15 +53,10 @@
                         </thead>
                         <tbody>
                         </tbody>
-
-
+                    </table>
                 </div>
-                <!-- Modal footer -->
-
-
             </div>
+            <!-- Modal footer -->
         </div>
     </div>
-
-    <!-- Extra Large Modal -->
-
+</div>
